ecf4191b616f6ff00d5d6d98e4d68a2925c1ebe0
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-01 13:32:52 -0300

    Completed project

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cards = document.querySelectorAll(".card"),
-timeTag = document.querySelector(".time b"),
-flipsTag = document.querySelector(".flips b"),
-refreshBtn = document.querySelector(".details button");
+  timeTag = document.querySelector(".time b"),
+  flipsTag = document.querySelector(".flips b"),
+  refreshBtn = document.querySelector(".details button");
 
 let maxTime = 20;
 let timeLeft = maxTime;
@@ -11,37 +11,38 @@ let disableDeck = false;
 let isPlaying = false;
 let cardOne, cardTwo, timer;
 
-function initTimer(){
-  if(timeLeft <= 0){
+function initTimer() {
+  if (timeLeft <= 0) {
     return clearInterval(timer);
   }
   timeLeft--;
   timeTag.innerText = timeLeft;
 }
 
-function flipCard({target: clickedCard}){
-  if(!isPlaying){
+function flipCard({ target: clickedCard }) {
+  if (!isPlaying) {
     isPlaying = true;
     timer = setInterval(initTimer, 1000);
   }
-  if(clickedCard !== cardOne && !disableDeck && timeLeft > 0){
+  if (clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
     flips++;
     flipsTag.innerText = flips;
     clickedCard.classList.add("flip");
-    if(!cardOne){
+    if (!cardOne) {
       return cardOne = clickedCard;
     }
     cardTwo = clickedCard;
+    disableDeck = true;
     let cardOneIcon = cardOne.querySelector(".back-view i").classList.value;
-    let cardTwoIcon = cardTwo.querySelector(".back-view i").classList.value;
+    cardTwoIcon = cardTwo.querySelector(".back-view i").classList.value;
     matchCards(cardOneIcon, cardTwoIcon);
   }
 }
 
-function matchCards(icon1, icon2){
-  if(icon1 === icon2){
+function matchCards(icon1, icon2) {
+  if (icon1 === icon2) {
     matchedCards++;
-    if(matchedCards == 6 && timeLeft > 0){
+    if (matchedCards == 6 && timeLeft > 0) {
       return clearInterval(timer);
     }
     cardOne.removeEventListener("click", flipCard);
@@ -50,12 +51,12 @@ function matchCards(icon1, icon2){
     return disableDeck = false;
   }
 
-  setTimeout(() =>{
+  setTimeout(() => {
     cardOne.classList.add("shake");
     cardTwo.classList.add("shake");
   }, 400);
 
-  setTimeout(() =>{
+  setTimeout(() => {
     cardOne.classList.remove("shake", "flip");
     cardTwo.classList.remove("shake", "flip");
     cardOne = cardTwo = "";
@@ -64,32 +65,33 @@ function matchCards(icon1, icon2){
 
 }
 
-function shuffleCards(){
+function shuffleCards() {
   timeLeft = maxTime;
   flips = matchedCards = 0;
-  cardOne = cardTwo = "";
+  cardOne = cardTwo = ""
   clearInterval(timer);
   timeTag.innerText = timeLeft;
   flipsTag.innerText = flips;
   disableDeck = isPlaying = false;
 
-  let arr = ["bxl-tiktok", "bxl-instagram-alt", "bxl-facebook-circle", "bxl-twitter", "bxl-whatsapp", "bxl-youtube"];
+  let arr = ["bxl-tiktok", "bxl-instagram-alt", "bxl-facebook-circle", "bxl-twitter", "bxl-whatsapp", "bxl-youtube", "bxl-tiktok", "bxl-instagram-alt", "bxl-facebook-circle", "bxl-twitter", "bxl-whatsapp", "bxl-youtube"];
   arr.sort(() => Math.random() > 0.5 ? 1 : -1);
 
-  cards.forEach((card, index) =>{
+  cards.forEach((card, index) => {
     card.classList.remove("flip");
     let iconTag = card.querySelector(".back-view i");
-    setTimeout(() =>{
+    setTimeout(() => {
       iconTag.classList.value = `bx ${arr[index]}`;
     }, 500);
     card.addEventListener("click", flipCard);
-  })
+  });
+
 }
 
 shuffleCards();
 
 refreshBtn.addEventListener("click", shuffleCards);
 
-cards.forEach(card =>{
+cards.forEach(card => {
   card.addEventListener("click", flipCard);
 });
